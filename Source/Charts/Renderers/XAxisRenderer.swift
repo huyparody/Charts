42fea33cb5f3330939f59b92fbeb473102782413
Trinh Duc Huy <huytd@educa.vn>
2022-08-19 09:10:23 +0700

    enable log

--- Source/Charts/Renderers/XAxisRenderer.swift
+++ Source/Charts/Renderers/XAxisRenderer.swift
@@ -304,7 +304,7 @@ open class XAxisRenderer: NSObject, AxisRenderer
             let indexLabelX = Int(Double(labelX) ?? 0)
             let label = axis.dates[indexLabelX - 1] //-1 vi i lech voi dateArray
             let labelns = label as NSString
-//            print(indexLabelX, dates[indexLabelX - 1], i)
+            print(indexLabelX, axis.dates[indexLabelX - 1], i)
             
             if axis.isAvoidFirstLastClippingEnabled
             {
